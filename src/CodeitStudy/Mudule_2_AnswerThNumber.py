857c080102b15d5fda6e1ecae0c65770adc42062
Jiwon Kim <devjiwonkim@users.noreply.github.com>
2019-04-09 00:05:32 -0700

    Update Mudule_2_AnswerThNumber.py
    연습문제 수정 후 제출_채점용

--- src/CodeitStudy/Mudule_2_AnswerThNumber.py
+++ src/CodeitStudy/Mudule_2_AnswerThNumber.py
@@ -6,28 +6,25 @@ Created on 2019. 4. 8.
 '''
 from random import randint
 
-count = 0
-Chance = 4
+tries = 0
+RandomNumber = randint(1,20)
 
-while count < Chance:
-    RandomNumber = randint(1,2)
-    guess = int(input("기회가  %d번 남았습니다. 1-20 사이의 숫자를 맞춰보세요 : " % (Chance-count)))
-    if count == 4:
-        print("아쉽습니다. 정답은 %d 였습니다." % (RandomNumber))
-        break
-    else:     
-        if guess == RandomNumber:
-            count += 1            
-            print("축하합니다. %d번만에 숫자를 맞추셨습니다." %(count))
+while tries < 4:
+    guess = int(input("기회가  %d번 남았습니다. 1-20 사이의 숫자를 맞춰보세요 : " % (4-tries)))
+    if RandomNumber > guess:
+        tries += 1
+        print("Up")
+        if tries == 4:
+            print("아쉽습니다. 정답은 %d였습니다." %(RandomNumber))
             break
-        elif guess > RandomNumber:
-            count += 1
-            Chance = Chance - count
-            print("Down")
-        elif guess < RandomNumber:
-            count += 1
-            Chance = Chance - count
-            print("Up")
-
+    elif RandomNumber <  guess:
+        tries += 1
+        print("Down")
+        if tries == 4:
+            print("아쉽습니다. 정답은 %d였습니다." %(RandomNumber))
+            break
+    else:
+        print("축하합니다. %d번 만에 숫자를 맞추셨습니다." % (tries+1))
+        break
 
         
